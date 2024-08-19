f112c0b4b1e0471b3d658ebac99070b008b30538
Fabian Schmidt <fabian.schmidt@hs-esslingen.de>
2024-08-19 09:11:48 +0200

    update docker container

--- .devcontainer/post_install_script.sh
+++ .devcontainer/post_install_script.sh
@@ -5,8 +5,11 @@ echo ">>> Start of post install script <<<"
 
 conda activate droidenv
 pip install evo --upgrade --no-binary evo
+pip install gdown
 # Install extensions
 #python setup.py install
-sudo -E env PATH=${PATH} python setup.py install
+
+cd droid-slam
+python setup.py install
 
 echo ">>> End of post install script <<<"
